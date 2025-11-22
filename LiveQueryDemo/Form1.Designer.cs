3273753e6b4480737cde41f448096ab08741b096
unicasoft <unicasoft@hotmail.com>
2025-11-22 13:58:44 +0300

    Update Form1.Designer.cs

--- LiveQueryDemo/Form1.Designer.cs
+++ LiveQueryDemo/Form1.Designer.cs
@@ -1,12 +1,13 @@
-
 using System.Windows.Forms;
+using System.Drawing;
 
 namespace LiveQueryDemo
 {
     partial class Form1
     {
-        private System.ComponentModel.IContainer components = null;
         private DataGridView grid;
+        private Panel panelTop;
+        private Panel panelButtons;
         private TextBox txtFirst;
         private TextBox txtLast;
         private TextBox txtTitle;
@@ -18,6 +19,7 @@ namespace LiveQueryDemo
         private Button btnPost;
         private Button btnCancel;
         private Button btnDelete;
+
         private TextBox txtLocate;
         private Button btnLocate;
         private TextBox txtFilter;
@@ -26,237 +28,154 @@ namespace LiveQueryDemo
         private Button btnOrderBy;
         private Label lblStatus;
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-
         private void InitializeComponent()
         {
-            this.grid = new System.Windows.Forms.DataGridView();
-            this.txtFirst = new System.Windows.Forms.TextBox();
-            this.txtLast = new System.Windows.Forms.TextBox();
-            this.txtTitle = new System.Windows.Forms.TextBox();
-            this.lblFirst = new System.Windows.Forms.Label();
-            this.lblLast = new System.Windows.Forms.Label();
-            this.lblTitle = new System.Windows.Forms.Label();
-            this.btnAppend = new System.Windows.Forms.Button();
-            this.btnEdit = new System.Windows.Forms.Button();
-            this.btnPost = new System.Windows.Forms.Button();
-            this.btnCancel = new System.Windows.Forms.Button();
-            this.btnDelete = new System.Windows.Forms.Button();
-            this.txtLocate = new System.Windows.Forms.TextBox();
-            this.btnLocate = new System.Windows.Forms.Button();
-            this.txtFilter = new System.Windows.Forms.TextBox();
-            this.btnFilter = new System.Windows.Forms.Button();
-            this.txtOrderBy = new System.Windows.Forms.TextBox();
-            this.btnOrderBy = new System.Windows.Forms.Button();
-            this.lblStatus = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.grid)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // grid
-            // 
-            this.grid.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.grid.Location = new System.Drawing.Point(12, 12);
-            this.grid.Name = "grid";
-            this.grid.Size = new System.Drawing.Size(760, 250);
-            this.grid.TabIndex = 0;
-            this.grid.SelectionChanged += new System.EventHandler(this.grid_SelectionChanged);
-            // 
-            // txtFirst
-            // 
-            this.txtFirst.Location = new System.Drawing.Point(90, 270);
-            this.txtFirst.Name = "txtFirst";
-            this.txtFirst.Size = new System.Drawing.Size(150, 20);
-            this.txtFirst.TabIndex = 1;
-            // 
-            // txtLast
-            // 
-            this.txtLast.Location = new System.Drawing.Point(90, 296);
-            this.txtLast.Name = "txtLast";
-            this.txtLast.Size = new System.Drawing.Size(150, 20);
-            this.txtLast.TabIndex = 2;
-            // 
-            // txtTitle
-            // 
-            this.txtTitle.Location = new System.Drawing.Point(90, 322);
-            this.txtTitle.Name = "txtTitle";
-            this.txtTitle.Size = new System.Drawing.Size(150, 20);
-            this.txtTitle.TabIndex = 3;
-            // 
-            // lblFirst
-            // 
-            this.lblFirst.AutoSize = true;
-            this.lblFirst.Location = new System.Drawing.Point(20, 273);
-            this.lblFirst.Name = "lblFirst";
-            this.lblFirst.Size = new System.Drawing.Size(57, 13);
-            this.lblFirst.TabIndex = 4;
-            this.lblFirst.Text = "FirstName:";
-            // 
-            // lblLast
-            // 
-            this.lblLast.AutoSize = true;
-            this.lblLast.Location = new System.Drawing.Point(20, 299);
-            this.lblLast.Name = "lblLast";
-            this.lblLast.Size = new System.Drawing.Size(58, 13);
-            this.lblLast.TabIndex = 5;
-            this.lblLast.Text = "LastName:";
-            // 
-            // lblTitle
-            // 
-            this.lblTitle.AutoSize = true;
-            this.lblTitle.Location = new System.Drawing.Point(20, 325);
-            this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Size = new System.Drawing.Size(30, 13);
-            this.lblTitle.TabIndex = 6;
-            this.lblTitle.Text = "Title:";
-            // 
-            // btnAppend
-            // 
-            this.btnAppend.Location = new System.Drawing.Point(260, 268);
-            this.btnAppend.Name = "btnAppend";
-            this.btnAppend.Size = new System.Drawing.Size(75, 23);
-            this.btnAppend.TabIndex = 7;
-            this.btnAppend.Text = "Append";
-            this.btnAppend.UseVisualStyleBackColor = true;
-            this.btnAppend.Click += new System.EventHandler(this.btnAppend_Click);
-            // 
-            // btnEdit
-            // 
-            this.btnEdit.Location = new System.Drawing.Point(341, 268);
-            this.btnEdit.Name = "btnEdit";
-            this.btnEdit.Size = new System.Drawing.Size(75, 23);
-            this.btnEdit.TabIndex = 8;
-            this.btnEdit.Text = "Edit";
-            this.btnEdit.UseVisualStyleBackColor = true;
-            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
-            // 
-            // btnPost
-            // 
-            this.btnPost.Location = new System.Drawing.Point(422, 268);
-            this.btnPost.Name = "btnPost";
-            this.btnPost.Size = new System.Drawing.Size(75, 23);
-            this.btnPost.TabIndex = 9;
-            this.btnPost.Text = "Post";
-            this.btnPost.UseVisualStyleBackColor = true;
-            this.btnPost.Click += new System.EventHandler(this.btnPost_Click);
-            // 
-            // btnCancel
-            // 
-            this.btnCancel.Location = new System.Drawing.Point(503, 268);
-            this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(75, 23);
-            this.btnCancel.TabIndex = 10;
-            this.btnCancel.Text = "Cancel";
-            this.btnCancel.UseVisualStyleBackColor = true;
-            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
-            // 
-            // btnDelete
-            // 
-            this.btnDelete.Location = new System.Drawing.Point(584, 268);
-            this.btnDelete.Name = "btnDelete";
-            this.btnDelete.Size = new System.Drawing.Size(75, 23);
-            this.btnDelete.TabIndex = 11;
-            this.btnDelete.Text = "Delete";
-            this.btnDelete.UseVisualStyleBackColor = true;
-            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
-            // 
-            // txtLocate
-            // 
-            this.txtLocate.Location = new System.Drawing.Point(260, 296);
-            this.txtLocate.Name = "txtLocate";
-            this.txtLocate.Size = new System.Drawing.Size(156, 20);
-            this.txtLocate.TabIndex = 12;
-            // 
-            // btnLocate
-            // 
-            this.btnLocate.Location = new System.Drawing.Point(422, 294);
-            this.btnLocate.Name = "btnLocate";
-            this.btnLocate.Size = new System.Drawing.Size(75, 23);
-            this.btnLocate.TabIndex = 13;
-            this.btnLocate.Text = "Locate";
-            this.btnLocate.UseVisualStyleBackColor = true;
-            this.btnLocate.Click += new System.EventHandler(this.btnLocate_Click);
-            // 
-            // txtFilter
-            // 
-            this.txtFilter.Location = new System.Drawing.Point(260, 322);
-            this.txtFilter.Name = "txtFilter";
-            this.txtFilter.Size = new System.Drawing.Size(156, 20);
-            this.txtFilter.TabIndex = 14;
-            // 
-            // btnFilter
-            // 
-            this.btnFilter.Location = new System.Drawing.Point(422, 320);
-            this.btnFilter.Name = "btnFilter";
-            this.btnFilter.Size = new System.Drawing.Size(75, 23);
-            this.btnFilter.TabIndex = 15;
-            this.btnFilter.Text = "Filter";
-            this.btnFilter.UseVisualStyleBackColor = true;
-            this.btnFilter.Click += new System.EventHandler(this.btnFilter_Click);
-            // 
-            // txtOrderBy
-            // 
-            this.txtOrderBy.Location = new System.Drawing.Point(260, 348);
-            this.txtOrderBy.Name = "txtOrderBy";
-            this.txtOrderBy.Size = new System.Drawing.Size(156, 20);
-            this.txtOrderBy.TabIndex = 16;
-            // 
-            // btnOrderBy
-            // 
-            this.btnOrderBy.Location = new System.Drawing.Point(422, 346);
-            this.btnOrderBy.Name = "btnOrderBy";
-            this.btnOrderBy.Size = new System.Drawing.Size(75, 23);
-            this.btnOrderBy.TabIndex = 17;
-            this.btnOrderBy.Text = "OrderBy";
-            this.btnOrderBy.UseVisualStyleBackColor = true;
-            this.btnOrderBy.Click += new System.EventHandler(this.btnOrderBy_Click);
-            // 
-            // lblStatus
-            // 
-            this.lblStatus.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.lblStatus.AutoSize = true;
-            this.lblStatus.Location = new System.Drawing.Point(12, 380);
-            this.lblStatus.Name = "lblStatus";
-            this.lblStatus.Size = new System.Drawing.Size(40, 13);
-            this.lblStatus.TabIndex = 18;
-            this.lblStatus.Text = "Status:";
-            // 
-            // Form1
-            // 
-            this.ClientSize = new System.Drawing.Size(784, 402);
-            this.Controls.Add(this.lblStatus);
-            this.Controls.Add(this.btnOrderBy);
-            this.Controls.Add(this.txtOrderBy);
-            this.Controls.Add(this.btnFilter);
-            this.Controls.Add(this.txtFilter);
-            this.Controls.Add(this.btnLocate);
-            this.Controls.Add(this.txtLocate);
-            this.Controls.Add(this.btnDelete);
-            this.Controls.Add(this.btnCancel);
-            this.Controls.Add(this.btnPost);
-            this.Controls.Add(this.btnEdit);
-            this.Controls.Add(this.btnAppend);
-            this.Controls.Add(this.lblTitle);
-            this.Controls.Add(this.lblLast);
-            this.Controls.Add(this.lblFirst);
-            this.Controls.Add(this.txtTitle);
-            this.Controls.Add(this.txtLast);
-            this.Controls.Add(this.txtFirst);
-            this.Controls.Add(this.grid);
-            this.Name = "Form1";
-            this.Text = "TLiveQuery Demo (.NET 4.6)";
+            this.grid = new DataGridView();
+            this.panelTop = new Panel();
+            this.panelButtons = new Panel();
+            this.txtFirst = new TextBox();
+            this.txtLast = new TextBox();
+            this.txtTitle = new TextBox();
+            this.lblFirst = new Label();
+            this.lblLast = new Label();
+            this.lblTitle = new Label();
+            this.btnAppend = new Button();
+            this.btnEdit = new Button();
+            this.btnPost = new Button();
+            this.btnCancel = new Button();
+            this.btnDelete = new Button();
+            this.txtLocate = new TextBox();
+            this.btnLocate = new Button();
+            this.txtFilter = new TextBox();
+            this.btnFilter = new Button();
+            this.txtOrderBy = new TextBox();
+            this.btnOrderBy = new Button();
+            this.lblStatus = new Label();
+
+            // Form
+            this.ClientSize = new System.Drawing.Size(900, 600);
+            this.Text = "TLiveQuery Demo (Modern UI)";
+            this.Font = new System.Drawing.Font("Segoe UI", 10F, FontStyle.Regular);
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.grid)).EndInit();
-            this.ResumeLayout(false);
-            this.PerformLayout();
+
+            // GRID
+            this.grid.Location = new Point(10, 10);
+            this.grid.Size = new Size(880, 330);
+            this.grid.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            this.grid.ReadOnly = false;
+            this.grid.AllowUserToAddRows = false;
+            this.grid.AllowUserToDeleteRows = false;
+            this.grid.ColumnHeadersDefaultCellStyle.BackColor = Color.LightGray;
+            this.grid.ColumnHeadersDefaultCellStyle.Font = new Font("Segoe UI", 10, FontStyle.Bold);
+            this.grid.EnableHeadersVisualStyles = false;
+            this.grid.SelectionChanged += new System.EventHandler(this.grid_SelectionChanged);
+            this.grid.AlternatingRowsDefaultCellStyle.BackColor = Color.FromArgb(245, 245, 245);
+
+            // PANEL TOP (Inputs)
+            this.panelTop.Location = new Point(10, 350);
+            this.panelTop.Size = new Size(880, 100);
+            this.panelTop.BorderStyle = BorderStyle.FixedSingle;
+
+            // FirstName
+            this.lblFirst.Text = "First:";
+            this.lblFirst.Location = new Point(20, 15);
+
+            this.txtFirst.Location = new Point(80, 12);
+            this.txtFirst.Size = new Size(180, 25);
+
+            // LastName
+            this.lblLast.Text = "Last:";
+            this.lblLast.Location = new Point(20, 50);
+
+            this.txtLast.Location = new Point(80, 47);
+            this.txtLast.Size = new Size(180, 25);
+
+            // Title
+            this.lblTitle.Text = "Title:";
+            this.lblTitle.Location = new Point(300, 15);
+
+            this.txtTitle.Location = new Point(350, 12);
+            this.txtTitle.Size = new Size(180, 25);
+
+            this.panelTop.Controls.AddRange(new Control[]
+            {
+                lblFirst, txtFirst,
+                lblLast, txtLast,
+                lblTitle, txtTitle
+            });
+
+            // PANEL BUTTONS
+            this.panelButtons.Location = new Point(10, 460);
+            this.panelButtons.Size = new Size(880, 50);
+
+            btnAppend.Text = "Append";
+            btnEdit.Text = "Edit";
+            btnPost.Text = "Post";
+            btnCancel.Text = "Cancel";
+            btnDelete.Text = "Delete";
+
+            Button[] btns = { btnAppend, btnEdit, btnPost, btnCancel, btnDelete };
+            int offset = 10;
+            foreach (var b in btns)
+            {
+                b.Size = new Size(100, 32);
+                b.Location = new Point(offset, 10);
+                b.BackColor = Color.FromArgb(240, 240, 240);
+                b.FlatStyle = FlatStyle.Flat;
+                offset += 110;
+            }
+
+            btnAppend.Click += btnAppend_Click;
+            btnEdit.Click += btnEdit_Click;
+            btnPost.Click += btnPost_Click;
+            btnCancel.Click += btnCancel_Click;
+            btnDelete.Click += btnDelete_Click;
+
+            this.panelButtons.Controls.AddRange(btns);
+
+            // Search / Filter / OrderBy
+            this.txtLocate.Location = new Point(10, 520);
+            this.txtLocate.Size = new Size(180, 25);
+            this.txtLocate.PlaceholderText = "Locate...";
+
+            this.btnLocate.Text = "Locate";
+            this.btnLocate.Location = new Point(200, 518);
+            this.btnLocate.Size = new Size(80, 28);
+            this.btnLocate.Click += btnLocate_Click;
+
+            this.txtFilter.Location = new Point(300, 520);
+            this.txtFilter.Size = new Size(180, 25);
+            this.txtFilter.PlaceholderText = "Filter...";
+
+            this.btnFilter.Text = "Apply";
+            this.btnFilter.Location = new Point(490, 518);
+            this.btnFilter.Size = new Size(80, 28);
+            this.btnFilter.Click += btnFilter_Click;
+
+            this.txtOrderBy.Location = new Point(590, 520);
+            this.txtOrderBy.Size = new Size(180, 25);
+            this.txtOrderBy.PlaceholderText = "OrderBy...";
+
+            this.btnOrderBy.Text = "Sort";
+            this.btnOrderBy.Location = new Point(780, 518);
+            this.btnOrderBy.Size = new Size(80, 28);
+            this.btnOrderBy.Click += btnOrderBy_Click;
+
+            // Status
+            this.lblStatus.Location = new Point(10, 555);
+            this.lblStatus.Size = new Size(880, 25);
+
+            // Form Controls
+            this.Controls.Add(this.grid);
+            this.Controls.Add(this.panelTop);
+            this.Controls.Add(this.panelButtons);
+            this.Controls.Add(this.txtLocate);
+            this.Controls.Add(this.btnLocate);
+            this.Controls.Add(this.txtFilter);
+            this.Controls.Add(this.btnFilter);
+            this.Controls.Add(this.txtOrderBy);
+            this.Controls.Add(this.btnOrderBy);
+            this.Controls.Add(this.lblStatus);
         }
     }
 }
